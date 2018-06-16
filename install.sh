09914f5eeeac48f854e052a0e6fe05a7b83c9c0c
xywei <wxy0516@gmail.com>
2018-06-16 11:56:35 +0800

    Add ranger conf

--- install.sh
+++ install.sh
@@ -6,4 +6,8 @@ mkdir -p $HOME/.mozilla
 emacs -Q --batch --eval '(require (quote org))' \
   --eval '(org-babel-tangle-file "conf.org")'
 
+# update X server database
 xrdb ~/.Xresources
+
+# Ranger file glyphs
+cd contrib/ranger_devicons && make install
