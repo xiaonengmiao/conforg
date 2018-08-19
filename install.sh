48709aaf0e78a1cd0011b2023f80a614df0e0fb2
xywei <wxy0516@gmail.com>
2018-08-19 01:23:21 -0500

    Create config directories

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 mkdir -p $HOME/.emacs.d
 mkdir -p $HOME/.mozilla
+mkdir -p $HOME/.config
+
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/i3
 
 emacs -Q --batch --eval '(require (quote org))' \
   --eval '(org-babel-tangle-file "conf.org")'
