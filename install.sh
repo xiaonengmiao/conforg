bc2bbf8d19ebf263aef0bb2833fe627f8558b0c9
xywei <wxy0516@gmail.com>
2018-10-14 12:19:59 -0500

    More informative install script

--- install.sh
+++ install.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
-mkdir -p $HOME/.emacs.d
-mkdir -p $HOME/.mozilla
-mkdir -p $HOME/.config
+LOGFILE=/tmp/conforg.log
 
-mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.config/i3
+function box_out() {
+  local s="$*"
+  tput setaf 3
+  echo " -${s//?/-}-
+| ${s//?/ } |
+| $(tput setaf 4)$s$(tput setaf 3) |
+| ${s//?/ } |
+ -${s//?/-}-"
+  tput sgr 0
+}
 
+box_out "Setting up directory structure.."
+# in a subshell
+(
+  set -x;
+  mkdir -p $HOME/.emacs.d;
+  mkdir -p $HOME/.mozilla;
+  mkdir -p $HOME/.config;
+
+  mkdir -p $HOME/.config/nvim;
+  mkdir -p $HOME/.config/i3;
+)
+
+box_out "Parsing conf.org.."
 emacs -Q --batch --eval '(require (quote org))' \
-  --eval '(org-babel-tangle-file "conf.org")'
+  --eval '(org-babel-tangle-file "conf.org")' \
+  >> $LOGFILE 2>&1
+tail -n 1 $LOGFILE
+echo "See $LOGFILE for details"
 
+box_out "Adding final touches.."
 # update X server database
-xrdb ~/.Xresources
+if ! [ -x "$(command -v xrdb)" ]; then
+  echo 'Warning: .Xresources is not activated.' >&2
+  set -o xtrace
+else
+  set -o xtrace
+  xrdb ~/.Xresources
+fi
 
 # Ranger file glyphs
-cd contrib/ranger_devicons && make install
+cd contrib/ranger_devicons && make install \
+  >> /tmp/conforg.log 2>&1
+
